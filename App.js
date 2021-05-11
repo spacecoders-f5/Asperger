a57ea57c02622141af017700ff4543173162e315
Ezequiel Gonzalez <zequi14@hotmail.com>
2021-05-11 20:10:18 +0200

    header component prepare

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 import  Navbar from './src/componentes/Navbar';
+import  Header from './src/componentes/Header';
 
 export default function App() {
   return (
     <View style={styles.container}>
 
+      <Header />
       <Navbar />
    
 
